de5ffd1ce38ab22f526d3ecfe451140acea3ceb0
“muhhjam” <muhhjam@gmail.com>
2023-11-15 11:10:06 +0700

    level 1

--- src/components/MyTweet.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="rounded p-5 my-2 border border-inherit text-center">
-		<!-- TODO: Uncomment baris kode dibawah untuk menampilkan informasi bahwa anda belum login -->
-		<h1 class="text-2xl">Anda belum login.</h1>
-		<p>Nantikan fitur login di level 2</p>
-	</div>
-</template>
--- src/views/MyTweetView.vue
+++ src/views/MyTweetView.vue
@@ -1,18 +1,13 @@
 <template>
 	<div class="flex justify-center bg-white h-screen px-5">
 		<div class="content px-5 py-5 bg-white h-full overflow-y-auto">
-			<MyTweet />
+			<div class="rounded p-5 my-2 border border-inherit text-center">
+				<!-- TODO: Uncomment baris kode dibawah untuk menampilkan informasi bahwa anda belum login -->
+				<h1 class="text-2xl">Anda belum login.</h1>
+				<p>Nantikan fitur login di level 2</p>
+			</div>
 		</div>
 	</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import MyTweet from '@/components/MyTweet.vue';
-
-export default {
-	components: {
-		MyTweet,
-	},
-};
-</script>
+<script></script>

9c41384ef18f2ce11395167718200b7780de8772
“muhhjam” <muhhjam@gmail.com>
2023-12-04 04:05:13 +0700

    level-3

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import './css/style.css';
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(far);
+library.add(fas, far);
 
 // Register the FontAwesomeIcon component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon);
